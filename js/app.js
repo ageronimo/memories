eb804b5c197421a5d84fa35de9c631c56f8fbe7c
ageronimo <angelageroniimo@gmail.com>
2017-02-01 09:19:48 -1000

    fix flipping & add other functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,31 @@
 var cardIsFlipped = false;
+var rotationStatus = [];
+var i;
 
 function flipCard(idName) {
-	//flips card to back
+  //flips card to back
 	var showBack = document.getElementById(idName);
-	showBack.style.transform = "rotateY(180deg)";
+  showBack.style.transform = "rotateY(180deg)";
+ 
+  //checks if two cards are flipped 
+  if (rotationStatus.length < 2) {
+    rotationStatus.push(idName);    
+  }
 
-	if (showBack.style.transform === "rotateY(180deg)") {
-		cardIsFlipped = true;
-		console.log("it's flipped!");
-	} 
-	if (cardIsFlipped === true) {
-		
-	}
+  if (rotationStatus.length == 2) {
+  document.getElementById(rotationStatus[0]).style.transform = "rotateY(0deg)";
+  document.getElementById(rotationStatus[1]).style.transform = "rotateY(0deg)";
+  }
+
+    // count--;
+  
+//   for (i = 0; i < rotationStatus.length; i++) {
+//     if (rotationStatus[i] == "unflipped"){
+//       showBack.style.transform = "rotateY(180deg)";
+//       console.log("unflipped");
+//     } else {
+//       showBack.style.transform = "rotateY(0deg)";
+//       console.log("flipped");
+//     }
+//   }
 }
