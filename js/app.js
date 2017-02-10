20cc778034c3e1aa1d42aadbc5e13a05927b2518
ageronimo <angelageroniimo@gmail.com>
2017-02-10 10:18:59 -1000

    finished app, need timer

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ var cardPics = [1,1,2,2,3,3,4,4];
 var rotationQueue = [];
 var picQueue = [];
 var i;
+var scored = [];
+var x = 0; //for scoreboard
 // var idMatch = document.getElementsByClassName("back")[i].id;
 
 function cardRandomizer() {
@@ -19,38 +21,62 @@ function cardRandomizer() {
       setImg.id = ""+cardPics[randomNum]+"";
 		}
 		cardPics.splice(randomNum, 1);
-    
 	}
+  document.getElementById("score").innerHTML = "Score: "+x;
+  document.getElementById("timer").innerHTML = "Time: currently unavailable";
 }
+//initializes above function
 cardRandomizer();
 
+//when a card is flipped:
 function flipCard(idName) {
+  //score board
+
   //flips card to back
-	var showBack = document.getElementById(idName);
+  var showBack = document.getElementById(idName);
   showBack.style.transform = "rotateY(180deg)";
  
-  var idMatch = showBack.getElementsByClassName("back")[0].id;
-  console.log(rotationQueue);
+
   //checks if two cards are flipped   
+  var idMatch = showBack.getElementsByClassName("back")[0].id;
   if(rotationQueue.length < 2){
     rotationQueue.push(idName); 
     picQueue.push(idMatch); 
     //^ after seeing that pic ids are sam   e, deletes divs, look up how to access id within div
   } 
-  if(rotationQueue.length == 2){
+  console.log(x);
+  if (rotationQueue.length == 2) {
+    setTimeout (function() {
+      if (picQueue[0] == picQueue[1]) {
+        // var deleteMe = document.getElementById(rotationQueue[0]);
+        // var deleteMe2 = document.getElementById(rotationQueue[1]);
+        // deleteMe.parentNode.removeChild(deleteMe);
+        // deleteMe2.parentNode.removeChild(deleteMe2);
+
+        document.getElementById(rotationQueue[0]).style.opacity = "0";
+        document.getElementById(rotationQueue[1]).style.opacity = "0";
+        
+        x = x+1; //inc score
+      }
+    }, 900);
     setTimeout(function(){
       document.getElementById(rotationQueue[0]).style.transform = "rotateY(0deg)";
       document.getElementById(rotationQueue[1]).style.transform = "rotateY(0deg)";
+      document.getElementById("score").innerHTML = "Score: "+x;
+      
+      // alerts if you won
+      if (x == 4) {
+        alert("You won! :D");
+      }
     }, 1000);
     // if (idMatch[0] !== idMatch[1]) {
     //   showBack.parentNode.removeChild(showBack); 
     // }
     setTimeout(function() {
       rotationQueue.splice(0, 2);
+      picQueue.splice(0, 2);
     }, 1050);
-
   }
-
 
 
 }
